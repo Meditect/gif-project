1e23fef42481a09ad47fecf404258b9b732a3ad1
Henri Gourgue <gourguehenri@gmail.com>
2023-02-09 00:03:20 +0100

    Added gif & see gif button to card

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 import DailyTrendsList from './components/daily-trend-list';
 
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, Flex, Heading, theme } from '@chakra-ui/react';
 
 import { QueryClient, QueryClientProvider } from "react-query";
 // import Axios from "axios";
@@ -20,6 +20,18 @@ function App() {
   return (
     <QueryClientProvider client={client}>
       <ChakraProvider theme={theme}>
+        <Flex
+          as="nav"
+          align="center"
+          justify="center"
+          wrap="wrap"
+          w="100%"
+          mb={1}
+          p={3}
+          bg={["primary.500", "primary.500", "transparent", "transparent"]}
+        >
+          <Heading>Daily Trends :</Heading>
+        </Flex>
         <DailyTrendsList></DailyTrendsList>
       </ChakraProvider>
     </QueryClientProvider>
--- src/components/daily-trend-list.tsx
+++ src/components/daily-trend-list.tsx
@@ -1,13 +1,13 @@
 import Axios from "axios";
 import { useQuery } from "react-query";
 
-import { Card, CardBody, CardFooter, Image, Text, Heading, Stack, Button } from '@chakra-ui/react';
+import { Card, CardBody, CardFooter, Image, Text, Heading, Stack, Button, Box, SimpleGrid } from '@chakra-ui/react';
 
 function DailyTrendsList() {
 
   const fetchTrends = async () => {
     const response = await Axios.get(
-      "http://localhost:3200"
+      "http://192.168.1.34:3200"
     );
     return response.data;
   };
@@ -19,40 +19,48 @@ function DailyTrendsList() {
 
   return (
     <div>
-      {data.map((item: any) => {
-        console.log(item)
-        return (
-          <Card style={{width: "20%"}}
-          key={item.title}
-            direction={{ base: 'column', sm: 'row' }}
-            overflow='hidden'
-            variant='outline'
-          >
+      <SimpleGrid minChildWidth='400px' spacingX='0px' spacingY='20px'>
+        {data.map((item: any, index: number) => {
+          console.log(item)
+          return (
+            <Box>
+              <Card style={{ margin: "10px" }}
+                key={item.title}
+                direction={{ base: 'column', sm: 'row' }}
+                overflow='hidden'
+                variant='outline'
+              >
 
-            <Image
-              objectFit='cover'
-              maxW={{ base: '100%', sm: '200px' }}
-              src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-              alt='Caffe Latte'
-            />
-            <Stack>
-              <CardBody>
-                <Heading size='md'>{item.title}</Heading>
+                <Image
+                  objectFit='cover'
+                  maxW={{ base: '100%', sm: '20%' }}
+                  maxH={{ base: '100%', sm: '40%' }}
+                  src='https://media1.giphy.com/media/Jcv1hzMf1LUnbfYYFm/giphy.gif?cid=ecf05e47dce4d853f2156f63b83d15e8386c546ea3c64ccf&rid=giphy.gif&ct=g'
+                  alt='Caffe Latte'
+                />
+                <Stack>
+                  <CardBody>
+                    <Heading size='md'>{index + 1} - {item.title}</Heading>
 
-                <Text py='2'>
-                  {item.nbSearch} recherches
-                </Text>
-              </CardBody>
+                    <Text py='2'>
+                      {item.nbSearch} research
+                    </Text>
+                  </CardBody>
 
-              <CardFooter >
-                <Button variant='solid' colorScheme='blue' size='xs'>
-                  <a href={"https://trends.google.fr" + item.link} target="_blank">Explore</a>
-                </Button>
-              </CardFooter>
-            </Stack>
-          </Card>
-        );
-      })}
+                  <CardFooter >
+                    <Button variant='solid' colorScheme='blue' size='xs'>
+                      <a href={"https://trends.google.fr" + item.link} target="_blank">Explore</a>
+                    </Button>
+                    <Button colorScheme='teal' size='xs' marginLeft={2}>
+                      <a href="https://media1.giphy.com/media/Jcv1hzMf1LUnbfYYFm/giphy.gif?cid=ecf05e47dce4d853f2156f63b83d15e8386c546ea3c64ccf&rid=giphy.gif&ct=g" target="_blank">See gif</a>
+                    </Button>
+                  </CardFooter>
+                </Stack>
+              </Card>
+            </Box>
+          );
+        })}
+      </SimpleGrid>
     </div>
 
   );
