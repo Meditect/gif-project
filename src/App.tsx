b1183cd67ccdc2261fc0d032e8e297515142debc
Henri Gourgue <gourguehenri@gmail.com>
2023-02-12 18:52:31 +0100

    Design for title bar and language select

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import DailyTrendsList from './components/daily-trend-list';
 import NavBar from './components/navbar';
 
-import { ChakraProvider, Flex, Heading, theme, Select } from '@chakra-ui/react';
+import { ChakraProvider, Flex, Heading, theme, Select, Box, Container, HStack, IconButton } from '@chakra-ui/react';
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { useTranslation } from 'react-i18next';
+import { FiTrendingUp } from 'react-icons/fi';
 
 function App() {
 
@@ -29,27 +30,48 @@ function App() {
     setCountryValue(newValue);
   }
 
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    }
+  }, []);
+
+  const isMobile = width <= 768;
+
   return (
     <QueryClientProvider client={client}>
       <ChakraProvider theme={theme}>
-        <NavBar></NavBar>
-        <Select onChange={handleSetSelectedOptions} variant="outline">
-          <option value='FR'>🇫🇷 France</option>
-          <option value='US'>🇺🇸 United States</option>
-        </Select>
-        <Flex
-          as="nav"
-          align="center"
-          justify="center"
-          wrap="wrap"
-          w="100%"
-          mb={1}
-          p={3}
-          bg={["primary.500", "primary.500", "transparent", "transparent"]}
-        >
-          <Heading>{t("title")} ({countryValue}) :</Heading>
-        </Flex>
-        <DailyTrendsList key={countryValue} countryValue={countryValue}></DailyTrendsList>
+        <div>
+          <Box
+            px={4}
+            position={"sticky"}
+            top={0}
+            boxShadow={"md"}
+            zIndex={2}
+            backgroundColor="white">
+            <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+              <HStack alignItems={"center"} spacing={4}>
+                <IconButton
+                  variant="ghost"
+                  icon={<FiTrendingUp fontSize="1.25rem" />}
+                  aria-label="Open Menu"
+                />
+                <Heading size='md'>{t("title")} ({countryValue})</Heading>
+              </HStack>
+              <Select onChange={handleSetSelectedOptions} variant="outline" maxWidth={"15%"}>
+                <option value='FR'>🇫🇷 France</option>
+                <option value='US'>🇺🇸 United States</option>
+              </Select>
+            </Flex>
+          </Box>
+        </div>
+        <div style={{ marginTop: "2%" }}><DailyTrendsList key={countryValue} countryValue={countryValue}></DailyTrendsList></div>
       </ChakraProvider>
     </QueryClientProvider>
   );
